86b6febce753440001a921ffcf362ffa69051fc0
Rizky Zakaria <rizky.zakaria78@gmail.com>
2023-12-20 11:46:20 +0700

    --allow-empty

--- app/Http/Controllers/Pegawai/ApelPagiController.php
+++ app/Http/Controllers/Pegawai/ApelPagiController.php
@@ -40,7 +40,7 @@ class ApelPagiController extends Controller
         // dishub prov 0.5999216, 123.0712941
         // kampus 4 0.5563289, 123.1317879
         //kost 0.5533161666666666, 123.145076
-        $distance = getDistance(0.5563289, 123.1317879, $request->lat, $request->long);
+        $distance = getDistance(0.5269178, 123.0562128, $request->lat, $request->long);
         if ($distance >= 300) {
             Alert::warning('Anda terlalu jauh dari kantor!');
             return redirect()->back();
